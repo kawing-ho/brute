8f8ca581d2c541a7ca0121f1d1a58ef57bc9137c
kawing-ho <kawing-ho@users.noreply.github.com>
2018-03-08 01:18:33 +1100

    added py file
    - Will tweak to make it faster 
    - Want to implement multi-threading ...

---
+++ brute.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+#enumerate and print until told to stop
+
+wordstr = "abcdefghijklmnopqrstuvwxyz0123456789" #/Change as necessary/
+l = len(wordstr)
+counters = [0]
+
+#recursively check the counter list from back to front
+def indexCheck(index):
+	
+	#base case
+	if index == 0:
+		if counters[index] == l:
+			counters[index] = 0
+			#if all counters reach maximum, add new counter
+			counters.append(0)
+		return 0
+	 
+	#else recurse down list
+	else : 
+		if counters[index] == l:
+			counters[index] = 0
+			counters[index-1] +=1
+		indexCheck(index-1)
+
+while True:	#forever and ever and ever and ever ...
+	
+	indexCheck(len(counters)-1)
+	
+	#loop through counters and add corresponding char to print string
+	p = ""
+	for index in counters: 
+		p += wordstr[index]
+		
+	#print the string
+	print counters,"\t",p
+	
+	#increase the counter  (current will always be counters[-1])
+	counters[-1] += 1
+	
